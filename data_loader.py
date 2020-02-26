f2065dd105322f378191bdb208d0f09b256db62b
Yimeng Zhu <yimeng.zhu0821@gmail.com>
2020-02-26 14:35:15 +0100

    fixed bugs in data_loader.py, added SpeechDataloader class

--- data_loader.py
+++ data_loader.py
@@ -4,17 +4,22 @@ from scipy.io.wavfile import read
 import librosa
 
 import torch
-from torch.utils.data import Dataset, Dataloader
+from torch.utils.data import Dataset, DataLoader
 import pandas
 
-class WavDataset(Dataset):
+class SpeechDataset(Dataset):
 
-    def __init__(self, csv_path, lexicon_path, sample_rate=16000,
+    def __init__(self, csv_path, lexicon_path='lexicon.json', sample_rate=16000,
                  window='hamming', window_size=.02, stride_size=.01):
         self.samples = pandas.read_csv(csv_path)
         with open(lexicon_path) as file:
             lexicon = str(''.join(json.load(file)))
         self.label = dict([(lexicon[i], i) for i in range(len(lexicon))])
+
+        self.sample_rate = sample_rate
+        self.window = window
+        self.window_size = window_size
+        self.stride_size = stride_size
 
     def __len__(self):
         return len(self.samples)
@@ -36,14 +41,14 @@ class WavDataset(Dataset):
         sound = sound.astype('float32') / 32767
         if len(sound.shape) > 1:
             sound = sound.squeeze() if sound.shape[1] == 1 else sound.mean(axis=1)
-        
+
         window_length = int(self.sample_rate * self.window_size)
         stride_length = int(self.sample_rate * self.stride_size)
 
         stft = librosa.stft(sound, n_fft=window_length,
                             hop_length=stride_length, win_length=window_length,
                            window = self.window)
-        
+
         magnitude, phase = librosa.magphase(stft)
 
         spect = torch.FloatTensor(np.log1p(magnitude))
@@ -61,3 +66,7 @@ class WavDataset(Dataset):
         transcript = list(filter(None, transcript))
         return transcript
 
+
+class SpeechDataloader(DataLoader):
+        def __init__(self, *args, **kwargs):
+            super(SpeechDataloader, self).__init__(*args, **kwargs)
